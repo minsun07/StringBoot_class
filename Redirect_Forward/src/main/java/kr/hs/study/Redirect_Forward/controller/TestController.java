f3ead0cfd315be893e2d4ca24b92c2b3d290222b
Minsun An <elleto123456@gmail.com>
2025-05-09 13:11:43 +0900

    Redirect와 Forward 기본 개념 및 흐름

--- Redirect_Forward/src/main/java/kr/hs/study/Redirect_Forward/controller/TestController.java
+++ Redirect_Forward/src/main/java/kr/hs/study/Redirect_Forward/controller/TestController.java
@@ -1,4 +1,23 @@
 package kr.hs.study.Redirect_Forward.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping("/forward")
+    public String forward(){
+        return "forward:/sub1"; // sub1의 주소로 이동해라
+                                // 얘는 주소!!
+    }
+
+    @GetMapping("/sub1")
+    public String sub1(){
+        return "sub1"; //얘는 타임리프 파일!!(html)
+    }
+
+    @GetMapping("/redirect")
+    public String redirect(){
+        return "redirect:/sub1";
+    }
 }
---
+++ Redirect_Forward/src/test/java/kr/hs/study/Redirect_Forward/RedirectForwardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.Redirect_Forward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectForwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
